d09e3357a443c8ebf204c3dd687ed65ce8688181
raohanan <117927984+raohanan@users.noreply.github.com>
2026-02-21 02:16:15 +0500

    frederick 4rth tanent done

--- functions/index.js
+++ functions/index.js
@@ -6,6 +6,8 @@ const { addAdmin } = require("./src/staff/addAdmin");
 const { assignCustomerClaims } = require("./src/staff/assignCustomerClaims");
 const { bootstrapAdminClaims } = require("./src/staff/bootstrapAdminClaims");
 const { createCheckoutSession } = require("./src/payments/createCheckoutSession");
+const { createCheckoutSessionForByrnActive } = require("./src/payments/createCheckoutSessionForByrnActive");
+const { createCheckoutSessionForFrederick } = require("./src/payments/createCheckoutSessionForFrederick");
 const { stripeWebhook } = require("./src/payments/stripeWebhook");
 
 // Export functions
@@ -17,6 +19,8 @@ exports.addAdmin = addAdmin;
 exports.assignCustomerClaims = assignCustomerClaims;
 exports.bootstrapAdminClaims = bootstrapAdminClaims;
 exports.createCheckoutSession = createCheckoutSession;
+exports.createCheckoutSessionForByrnActive = createCheckoutSessionForByrnActive;
+exports.createCheckoutSessionForFrederick = createCheckoutSessionForFrederick;
 exports.stripeWebhook = stripeWebhook;
 
 // Example of other potential exports
